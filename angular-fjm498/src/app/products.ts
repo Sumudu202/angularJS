8a756e12ef70f2642a92b0c9bd1d5d1a2dd25327
Sumudu Manic Purage <sumudurajith@gmail.com>
2019-07-29 10:15:40 +1200

    Add files via upload

---
+++ angular-fjm498/src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Samsung Galaxy S10e',
+    price: 1400,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Samsung Galaxy S10',
+    price: 1200,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Samsung Galaxy S9',
+    price: 800,
+    description: ''
+  },
+  {
+    name: 'huawei p30 pro',
+    price: 1300,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
